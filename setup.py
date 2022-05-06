3aee2a2178d7041ea111a3237221d12dfd788b71
Pat Nadolny <patnadolny@gmail.com>
2022-05-06 16:00:13 -0400

    reference hotglue netsuite sdk version
    reference hotglue netsuite sdk version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     py_modules=['tap_netsuite'],
     install_requires=[
-        'netsuitesdk==2.7.4', # USING THE HOTGLUE VERSION
+        'git+https://github.com/hotgluexyz/netsuite-sdk-py.git@2.7.4', # USING THE HOTGLUE VERSION
         'requests==2.21.0',
         'singer-python==5.3.1',
         'xmltodict==0.11.0',
