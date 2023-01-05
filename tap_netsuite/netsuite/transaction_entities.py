2474bb96b0a434d9701b6d1ac30973f63198eb05
Josué M. Sehnem <josue@sehnem.com>
2023-01-05 17:06:12 -0500

    add InboundShipment

--- tap_netsuite/netsuite/transaction_entities.py
+++ tap_netsuite/netsuite/transaction_entities.py
@@ -53,6 +53,26 @@ class Customers(ApiBase):
     def post(self, data) -> OrderedDict:
         return None
 
+
+class InboundShipment(ApiBase):
+    def __init__(self, ns_client):
+        ApiBase.__init__(self, ns_client=ns_client, type_name='InboundShipment')
+        self.require_lastModified_date = True
+
+    def get_all(self, last_modified_date=None):
+        return self.get_all_generator(last_modified_date=last_modified_date)
+
+    def get_all_generator(self, page_size=200, last_modified_date=None):
+        search_record = self.ns_client.basic_search_factory(type_name="InboundShipment",
+                                                            lastModifiedDate=last_modified_date)
+        ps = PaginatedSearch(client=self.ns_client, type_name='InboundShipment', pageSize=page_size,
+                             search_record=search_record)
+        return self._paginated_search_to_generator(ps)
+
+    def post(self, data) -> OrderedDict:
+        return None
+
+
 class InventoryItem(ApiBase):
     def __init__(self, ns_client):
         ApiBase.__init__(self, ns_client=ns_client, type_name='InventoryItem')
